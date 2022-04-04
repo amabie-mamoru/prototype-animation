4da54aa583be1d7493cb8292fc0178ec7153aa4e
amabie-mamoru <amabie.mamoru@gmail.com>
2022-04-05 01:57:30 +0900

    simple animation

--- PrototypeAnimation/Assets/Scripts/DebugUI.cs
+++ PrototypeAnimation/Assets/Scripts/DebugUI.cs
@@ -15,10 +15,12 @@ public class DebugUI : MonoBehaviour
 
     public void OnExclamationMarkBtnClicked()
     {
-        var mark = Resources.Load<Sprite>("Sprites/exclamation-mark");
-        var gameObj = new GameObject("Mark");
-        var spriteRenderer = gameObj.AddComponent<SpriteRenderer>();
-        spriteRenderer.sprite = mark;
+        var prefab = Resources.Load<GameObject>("Prefabs/ExclamationMark");
+        var gameObj = Instantiate(prefab);
+        //var gameObj = new GameObject("Mark");
+        //var spriteRenderer = gameObj.AddComponent<SpriteRenderer>();
+        //spriteRenderer.sprite = mark;
+        var mark = gameObj.GetComponent<SpriteRenderer>();
         gameObj.transform.localScale = new Vector3(1, 1, 1);
         gameObj.transform.position = new Vector3(gameObj.transform.position.x - mark.bounds.size.x / 2, gameObj.transform.position.y + mark.bounds.size.y / 2, 0);
         gameObj.transform.SetParent(uiCanvas.transform);
