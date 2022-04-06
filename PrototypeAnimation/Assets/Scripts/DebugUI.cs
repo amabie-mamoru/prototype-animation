eab935cf5a1a2d4236e6b005684847e22ef8d1d4
amabie-mamoru <amabie.mamoru@gmail.com>
2022-04-07 01:04:13 +0900

    add root object and add post processing

--- PrototypeAnimation/Assets/Scripts/DebugUI.cs
+++ PrototypeAnimation/Assets/Scripts/DebugUI.cs
@@ -4,8 +4,11 @@ using UnityEngine.UIElements;
 public class DebugUI : MonoBehaviour
 {
     [SerializeField] private UIDocument uiDocument;
-    [SerializeField] private Canvas spriteCanvas;
-
+    // Canvas を直接参照すると Camera - Overlay の場合 Anchor が左下固定のため
+    // 追加するオブジェクトも全てアンカーを左下にしないといけなくなる
+    // それをあえて避けるために Canvas アンカーを左下とする Root となる GameObject を定義することで
+    // 追加される GameObject の Anchor をデフォルト(中央)で定義しやすくするための工夫
+    [SerializeField] private GameObject spriteRoot;
     private void Awake()
     {
         var rootVisualElement = uiDocument.rootVisualElement;
@@ -23,6 +26,6 @@ public class DebugUI : MonoBehaviour
         var mark = gameObj.GetComponent<SpriteRenderer>();
         gameObj.transform.localScale = new Vector3(10, 10, 1); // TODO: localScale はたぶん UI の解像度に合わせて変更する処理が必要
         gameObj.transform.position = new Vector3(gameObj.transform.position.x - mark.bounds.size.x / 2, gameObj.transform.position.y + mark.bounds.size.y / 2, 0);
-        gameObj.transform.SetParent(spriteCanvas.transform);
+        gameObj.transform.SetParent(spriteRoot.transform);
     }
 }
